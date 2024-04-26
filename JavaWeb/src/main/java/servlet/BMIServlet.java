54c83815fa3f6a1e11eaaa457017bb5434bedcf7
NTPU <NTPU@503-tr>
2024-04-26 13:46:02 +0800

    web

--- JavaWeb/src/main/java/servlet/BMIServlet.java
+++ JavaWeb/src/main/java/servlet/BMIServlet.java
@@ -17,8 +17,19 @@ public class BMIServlet extends HttpServlet {
 		resp.setCharacterEncoding("utf-8");
 		resp.setContentType("text/html;charset=utf-8");
 		
+		// 接收參數
+		String userName = req.getParameter("userName");
+		String userHeight = req.getParameter("userHeight");
+		String userWeight = req.getParameter("userWeight");
+		
+		// 計算 BMI
+		// 將 userHeight 與 userWeight 由 String 轉 Double
+		double height = Double.parseDouble(userHeight);
+		double weight = Double.parseDouble(userWeight);
+		double bmi = weight / Math.pow(height/100, 2);
+		
 		// 回應內容:
-		resp.getWriter().print("BMI Result = ");
+		resp.getWriter().print("BMI Result = " + bmi);
 		
 	}
 	
