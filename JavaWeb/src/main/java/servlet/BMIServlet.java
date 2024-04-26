e58f47e231d90c94f0a2053c619d3827008817f5
NTPU <NTPU@503-tr>
2024-04-26 15:32:21 +0800

    java

--- JavaWeb/src/main/java/servlet/BMIServlet.java
+++ JavaWeb/src/main/java/servlet/BMIServlet.java
@@ -1,6 +1,9 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -10,10 +13,28 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet("/servlet/bmi")
 public class BMIServlet extends HttpServlet {
+	
+	// 存放 bmi 的歷史紀錄
+	private List<Map<String, Object>> bmiList = new CopyOnWriteArrayList();
+	
+	// 查詢使用
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// 編碼
+		req.setCharacterEncoding("utf-8");
+		resp.setCharacterEncoding("utf-8");
+		resp.setContentType("text/html;charset=utf-8");
+		
+		// 回應內容:
+		resp.getWriter().print(bmiList);
+	}
+	
+	// 表單使用
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// 編碼
+		req.setCharacterEncoding("utf-8");
 		resp.setCharacterEncoding("utf-8");
 		resp.setContentType("text/html;charset=utf-8");
 		
