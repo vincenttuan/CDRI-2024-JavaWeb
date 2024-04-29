1e12499403d70611aceb0d46090e5ce8a66135a2
NTPU <NTPU@503-tr>
2024-04-29 14:25:08 +0800

    web

--- JavaWeb/src/main/java/guestbook/controller/GuestBookController.java
+++ JavaWeb/src/main/java/guestbook/controller/GuestBookController.java
@@ -1,9 +1,30 @@
 package guestbook.controller;
 
+import java.io.IOException;
+
+import guestbook.service.GuestBookService;
+import guestbook.service.GuestBookServiceImpl;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet("/guestbook")
 public class GuestBookController extends HttpServlet {
+	
+	private GuestBookService service = new GuestBookServiceImpl();
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doPost(req, resp);
+	}
 	
 }
