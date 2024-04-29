7e87237df6903b927edc09a19ce1f0ce8777c5aa
NTPU <NTPU@503-tr>
2024-04-29 14:33:16 +0800

    java

--- JavaWeb/src/main/java/guestbook/controller/GuestBookController.java
+++ JavaWeb/src/main/java/guestbook/controller/GuestBookController.java
@@ -1,9 +1,12 @@
 package guestbook.controller;
 
 import java.io.IOException;
+import java.util.List;
 
+import guestbook.model.GuestBook;
 import guestbook.service.GuestBookService;
 import guestbook.service.GuestBookServiceImpl;
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -17,14 +20,26 @@ public class GuestBookController extends HttpServlet {
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
-		
+		// 取得所有留言紀錄
+		List<GuestBook> guestBooks = service.queryAll();
+		req.setAttribute("guestBooks", guestBooks);
+		// 內重導到 jsp
+		RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/jsp/guestbook.jsp");
+		rd.forward(req, resp);
 	}
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		super.doPost(req, resp);
+		String username = req.getParameter("username");
+		String content = req.getParameter("content");
+		
+		boolean state = service.add(username, content);
+		req.setAttribute("state", state);
+		
+		// 內重導到 jsp
+		RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/jsp/guestbook_result.jsp");
+		rd.forward(req, resp);
+		
 	}
 	
 }
