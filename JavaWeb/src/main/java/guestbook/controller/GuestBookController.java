9cb62243a39834a6f5c5b6f743d1445c86a5dde7
NTPU <NTPU@503-tr>
2024-04-29 14:20:40 +0800

    web

---
+++ JavaWeb/src/main/java/guestbook/controller/GuestBookController.java
@@ -0,0 +1,9 @@
+package guestbook.controller;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet("/guestbook")
+public class GuestBookController extends HttpServlet {
+	
+}
