aeb77447c6275e3fc93f20c7b428989c3d5234a1
NTPU <NTPU@503-tr>
2024-04-29 12:22:38 +0800

    web

--- JavaWeb/src/main/java/guestbook/model/GuestBook.java
+++ JavaWeb/src/main/java/guestbook/model/GuestBook.java
@@ -2,13 +2,21 @@ package guestbook.model;
 
 import java.util.Date;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class GuestBook {
 	private Integer id;
 	private String username;
 	private String content;
 	private Date createDate;
 	private Date updateDate;
+	
+	public GuestBook(String username, String content) {
+		this.username = username;
+		this.content = content;
+	}
 }
