eda9b758d3faa3d1b72cf0c06fa5d10c5362d2bc
NTPU <NTPU@503-tr>
2024-05-01 15:51:13 +0800

    web

--- JavaWeb/src/main/java/room/dao/BookingRoomDao.java
+++ JavaWeb/src/main/java/room/dao/BookingRoomDao.java
@@ -2,12 +2,15 @@ package room.dao;
 
 import java.util.List;
 
+import room.model.dto.BookingRoomCount;
 import room.model.po.BookingRoom;
 
 public interface BookingRoomDao {
 	BookingRoom findById(Integer bookingId);
 	List<BookingRoom> findAll();
 	List<BookingRoom> findByUserId(Integer userId);
+	List<BookingRoomCount> getBookingRoomCounts(); // 計算每個房間的預訂次數
+	
 	int create(BookingRoom bookingRoom);
 	int update(Integer bookingId, BookingRoom bookingRoom);
 	int delete(Integer bookingId);
