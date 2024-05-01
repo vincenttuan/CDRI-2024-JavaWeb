c0fc2be4975fccf97359d8b5155e3dd0515aac68
NTPU <NTPU@503-tr>
2024-05-01 11:23:52 +0800

    web

--- JavaWeb/src/main/java/room/dao/BookingRoomDaoImpl.java
+++ JavaWeb/src/main/java/room/dao/BookingRoomDaoImpl.java
@@ -23,32 +23,32 @@ public class BookingRoomDaoImpl implements BookingRoomDao {
 
 	@Override
 	public List<BookingRoom> findAll() {
-		
-		return null;
+		String sql = "SELECT booking_id, room_id, user_id, checkin_date, create_time FROM booking_room";
+		return jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(BookingRoom.class));
 	}
 
 	@Override
 	public List<BookingRoom> findByUserId(Integer userId) {
-		// TODO Auto-generated method stub
-		return null;
+		String sql = "SELECT booking_id, room_id, user_id, checkin_date, create_time FROM booking_room WHERE user_id = ?";
+		return jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(BookingRoom.class), userId);
 	}
 
 	@Override
 	public int create(BookingRoom bookingRoom) {
-		// TODO Auto-generated method stub
-		return 0;
+		String sql = "INSERT INTO booking_room (room_id, user_id, checkin_date) VALUES (?, ?, ?)";
+		return jdbcTemplate.update(sql, bookingRoom.getRoomId(), bookingRoom.getUserId(), bookingRoom.getCheckinDate());
 	}
 
 	@Override
 	public int update(Integer bookingId, BookingRoom bookingRoom) {
-		// TODO Auto-generated method stub
-		return 0;
+		String sql = "UPDATE booking_room SET room_id = ?, user_id = ?, checkin_date = ? WHERE booking_id = ?";
+		return jdbcTemplate.update(sql, bookingRoom.getRoomId(), bookingRoom.getUserId(), bookingRoom.getCheckinDate(), bookingId);
 	}
 
 	@Override
 	public int delete(Integer bookingId) {
-		// TODO Auto-generated method stub
-		return 0;
+		String sql = "DELETE FROM booking_room WHERE booking_id = ?";
+		return jdbcTemplate.update(sql, bookingId);
 	}
 
 }
