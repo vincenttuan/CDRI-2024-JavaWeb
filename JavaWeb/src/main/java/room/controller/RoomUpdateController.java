976a6c1d0d4e6c041eadde5d2ec43d29762179df
NTPU <NTPU@503-tr>
2024-05-01 12:06:52 +0800

    web

---
+++ JavaWeb/src/main/java/room/controller/RoomUpdateController.java
@@ -0,0 +1,39 @@
+package room.controller;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import room.service.RoomService;
+
+// 進行 Room 修改
+@WebServlet("/room/update")
+public class RoomUpdateController extends HttpServlet {
+	private RoomService service = new RoomService();
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// 取得 Room ID
+		Integer roomId = Integer.parseInt(req.getParameter("roomId"));
+		// 取得 Room 資料
+		req.setAttribute("room", service.getRoom(roomId));
+		// 轉發至 Room 修改頁面
+		req.getRequestDispatcher("/WEB-INF/views/room/update.jsp").forward(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// 取得 Room ID
+		Integer roomId = Integer.parseInt(req.getParameter("roomId"));
+		// 取得 Room 名稱
+		String roomName = req.getParameter("roomName");
+		// 取得 Room 資料
+		service.updateRoom(roomId, roomName);
+		// 重新導向至 Room 主頁面
+		resp.sendRedirect(req.getContextPath() + "/room");
+	}
+	
+}
