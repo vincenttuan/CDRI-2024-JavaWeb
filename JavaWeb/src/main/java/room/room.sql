66b9fa10d40c1bd901186f6103dd733889dd91f6
NTPU <NTPU@503-tr>
2024-05-01 09:55:52 +0800

    web

--- JavaWeb/src/main/java/room/room.sql
+++ JavaWeb/src/main/java/room/room.sql
@@ -1,21 +1,21 @@
 -- 訂房系統
 
-drop table if exists BookingRoom;
-drop table if exists Room;
+drop table if exists booking_room;
+drop table if exists room;
 
--- 建立 Room
-create table if not exists Room(
+-- 建立 room
+create table if not exists room(
 	room_id int primary key,
     room_name varchar(50) not null unique
 );
 
--- 建立 BookingRoom
-create table if not exists BookingRoom(
+-- 建立 booking_room
+create table if not exists booking_room(
 	booking_id int auto_increment primary key,
     room_id int not null,
     user_id int not null,
-    checkin_date date not null,
+    checkin_date timestamp not null,
     create_time timestamp default current_timestamp,
-    foreign key (room_id) references Room(room_id),
+    foreign key (room_id) references room(room_id),
     CONSTRAINT unique_room_id_and_checkin_date UNIQUE(room_id, checkin_date)
 );
