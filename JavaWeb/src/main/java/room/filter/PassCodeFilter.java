1f29995e4756dd833d1d9e6111393ce73d3240fe
NTPU <NTPU@503-tr>
2024-05-03 15:18:08 +0800

    webfilter

---
+++ JavaWeb/src/main/java/room/filter/PassCodeFilter.java
@@ -0,0 +1,24 @@
+package room.filter;
+
+import java.io.IOException;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebFilter("/room/*")
+public class PassCodeFilter extends HttpFilter {
+
+	@Override
+	protected void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		
+		System.out.println("PassCodeFilter for Room");
+		chain.doFilter(request, response);
+		
+	}
+	
+}
