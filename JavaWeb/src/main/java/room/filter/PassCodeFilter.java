b47e680997f935a120a020d3ffcd3c54bfe1f6db
NTPU <NTPU@503-tr>
2024-05-03 15:28:48 +0800

    webfilter

--- JavaWeb/src/main/java/room/filter/PassCodeFilter.java
+++ JavaWeb/src/main/java/room/filter/PassCodeFilter.java
@@ -8,6 +8,7 @@ import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 @WebFilter("/room/*")
 public class PassCodeFilter extends HttpFilter {
@@ -16,8 +17,19 @@ public class PassCodeFilter extends HttpFilter {
 	protected void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
 		
-		System.out.println("PassCodeFilter for Room");
-		chain.doFilter(request, response);
+		HttpSession session = request.getSession(); // 取得 session 物件
+		System.out.println("PassCodeFilter for Room, sessionId = " + session.getId());
+		// 檢查 request 是否有帶入 ?code=1234
+		String code = request.getParameter("code") == null? "" : request.getParameter("code");
+		if(code.equals("1234")) { // 檢查參數 code 是否等於 1234
+			// 將 code 的值寫入到 session 屬性中
+			session.setAttribute("code", code);
+			chain.doFilter(request, response); // pass
+		} else if("1234".equals(session.getAttribute("code")+"")) { // 檢查 session["code"] 的內容是否是 1234
+			chain.doFilter(request, response); // pass
+		} else {
+			response.getWriter().print("PassCode Error !");
+		}
 		
 	}
 	
