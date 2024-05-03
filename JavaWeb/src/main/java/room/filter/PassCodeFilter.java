3e3a4e5689560b82c86e903197671af5f975ac1d
NTPU <NTPU@503-tr>
2024-05-03 15:45:19 +0800

    webfilter

--- JavaWeb/src/main/java/room/filter/PassCodeFilter.java
+++ JavaWeb/src/main/java/room/filter/PassCodeFilter.java
@@ -10,7 +10,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
-@WebFilter("/room/*")
+@WebFilter(urlPatterns = {"/room/*", "/bookingroom/*"})
 public class PassCodeFilter extends HttpFilter {
 
 	@Override
