178629d571069ebdf1af3a0c79b07bc21ac224ec
NTPU <NTPU@503-tr>
2024-05-06 14:16:14 +0800

    spring

--- SpringCore/src/session02/test/TestBeverage.java
+++ SpringCore/src/session02/test/TestBeverage.java
@@ -1,14 +1,18 @@
 package session02.test;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import session02.bean.BeverageMachine;
+import session02.config.BeverageConfig;
 
+// 使用 java	配置在執行時要加上 VM 參數: --add-opens java.base/java.lang=ALL-UNNAMED
 public class TestBeverage {
 
 	public static void main(String[] args) {
-		ApplicationContext ctx = new ClassPathXmlApplicationContext("beans-config3.xml");
+		//ApplicationContext ctx = new ClassPathXmlApplicationContext("beans-config3.xml");
+		ApplicationContext ctx = new AnnotationConfigApplicationContext(BeverageConfig.class);
 		BeverageMachine machine1 = ctx.getBean("coffeeBeverageMachine", BeverageMachine.class);
 		System.out.println(machine1.serveBeverage());
 		BeverageMachine machine2 = ctx.getBean("teaBeverageMachine", BeverageMachine.class);
