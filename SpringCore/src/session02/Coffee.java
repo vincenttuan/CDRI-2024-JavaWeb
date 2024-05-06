b5e2704da1fd9e3c20731f475452db1e607e096f
NTPU <NTPU@503-tr>
2024-05-06 13:47:20 +0800

    spring

---
+++ SpringCore/src/session02/Coffee.java
@@ -0,0 +1,10 @@
+package session02;
+
+public class Coffee implements Beverage {
+
+	@Override
+	public String serve() {
+		return "一杯咖啡";
+	}
+	
+}
---
+++ SpringCore/src/session02/Tea.java
@@ -0,0 +1,10 @@
+package session02;
+
+public class Tea implements Beverage {
+
+	@Override
+	public String serve() {
+		return "一壺熱茶";
+	}
+	
+}
