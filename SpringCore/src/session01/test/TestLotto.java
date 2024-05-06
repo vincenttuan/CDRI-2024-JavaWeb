5d62e529c29ffdb6ddd81b19b6e78b490d4289c3
NTPU <NTPU@503-tr>
2024-05-06 11:53:20 +0800

    spring lotto

--- SpringCore/src/session01/test/TestLotto.java
+++ SpringCore/src/session01/test/TestLotto.java
@@ -1,10 +1,17 @@
 package session01.test;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import session01.bean.Lotto;
+
 public class TestLotto {
 
 	public static void main(String[] args) {
 		// 利用 Spring 得到 Lotto 號碼並印出 
-		
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("beans-config.xml");
+		Lotto lotto = ctx.getBean("lotto", Lotto.class);
+		System.out.println(lotto.getNumbers());
 	}
 
 }
