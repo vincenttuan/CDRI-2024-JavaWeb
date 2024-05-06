b24fbd6dd7f540968a4c4428f9c99bdcbf18d667
NTPU <NTPU@503-tr>
2024-05-06 16:16:58 +0800

    spring

---
+++ SpringCore/src/session04/config/FoodConfig.java
@@ -0,0 +1,10 @@
+package session04.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "session04.bean")
+public class FoodConfig {
+
+}
