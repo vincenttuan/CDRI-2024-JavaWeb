1c957804e94546b7075f15356f7ae48c9fd7891e
NTPU <NTPU@503-tr>
2024-05-06 15:26:09 +0800

    spring

---
+++ SpringCore/src/session03/test/TestBook2.java
@@ -0,0 +1,18 @@
+package session03.test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import session03.bean.LibraryManager;
+
+public class TestBook2 {
+
+	public static void main(String[] args) {
+		// 使用 Spring
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("book-config.xml");
+		LibraryManager libraryManager = ctx.getBean("libraryManager", LibraryManager.class);
+		libraryManager.displayComic();
+		libraryManager.displayComputer();
+	}
+
+}
