8c85f517afbfd82a53b16655824076c61be3a881
NTPU <NTPU@503-tr>
2024-05-06 14:31:38 +0800

    spring

--- SpringCore/src/session02/test/TestBeverage.java
+++ SpringCore/src/session02/test/TestBeverage.java
@@ -9,7 +9,7 @@ import session02.bean.BeverageMachine;
 public class TestBeverage {
 
 	public static void main(String[] args) {
-		ApplicationContext ctx = new ClassPathXmlApplicationContext("beans-config3.xml");
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("beverage-config.xml");
 		//ApplicationContext ctx = new AnnotationConfigApplicationContext(BeverageConfig.class);
 		BeverageMachine machine1 = ctx.getBean("coffeeBeverageMachine", BeverageMachine.class);
 		System.out.println(machine1.serveBeverage());
