958d4ce3482f8a8c124b9a5622122d50ae397b99
NTPU <NTPU@503-tr>
2024-05-06 10:59:22 +0800

    hello

--- SpringCore/src/session01/test/TestHello.java
+++ SpringCore/src/session01/test/TestHello.java
@@ -5,7 +5,7 @@ import session01.bean.Hello;
 public class TestHello {
 
 	public static void main(String[] args) {
-		// 傳統: 自行管理物件
+		// 傳統: 自行管理物件, 自行 new
 		Hello hello1 = new Hello();
 		System.out.println(hello1.getToday());
 
