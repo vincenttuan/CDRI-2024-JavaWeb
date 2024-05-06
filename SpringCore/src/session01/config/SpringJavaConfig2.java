f868715310a43946e087866e417f4104471f82df
NTPU <NTPU@503-tr>
2024-05-06 12:26:45 +0800

    spring

---
+++ SpringCore/src/session01/config/SpringJavaConfig2.java
@@ -0,0 +1,10 @@
+package session01.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = {"session01.bean"})
+public class SpringJavaConfig2 {
+
+}
