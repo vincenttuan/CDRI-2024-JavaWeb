484a1b61068143f3758b30f89509ad5e38b4ab4b
NTPU <NTPU@503-tr>
2024-05-06 13:50:15 +0800

    spring

---
+++ SpringCore/src/session02/BeverageMachine.java
@@ -0,0 +1,25 @@
+package session02;
+
+public class BeverageMachine {
+	private Beverage beverage;
+	
+	public BeverageMachine() {
+		
+	}
+	
+	// 建構子注入
+	public BeverageMachine(Beverage beverage) {
+		this.beverage = beverage;
+	}
+
+	public String serveBeverage() {
+		return beverage.serve();
+	}
+	
+	// 方法注入
+	public void setBeverage(Beverage beverage) {
+		this.beverage = beverage;
+	}
+	
+	
+}
