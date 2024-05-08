4c9ccfc02971f023e1683385e44cb0070afe429e
NTPU <NTPU@503-tr>
2024-05-08 09:26:29 +0800

    spring

---
+++ SpringCore/src/session05/service/GuestBookService2Impl.java
@@ -0,0 +1,48 @@
+package session05.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import session05.bean.GuestBook;
+
+@Service("gbService2")
+public class GuestBookService2Impl implements GuestBookService {
+
+	@Override
+	public boolean add(String username, String content) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public GuestBook getById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<GuestBook> queryAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean updateUsername(Integer id, String username) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean updateContent(Integer id, String content) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean removeById(Integer id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
