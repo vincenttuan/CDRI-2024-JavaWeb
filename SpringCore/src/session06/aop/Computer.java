049a8457b5594c1feae2b7a687f25bc3c490cf40
NTPU <NTPU@503-tr>
2024-05-08 14:06:19 +0800

    aop

---
+++ SpringCore/src/session06/aop/Computer.java
@@ -0,0 +1,8 @@
+package session06.aop;
+
+public interface Computer {
+	Integer add(Integer x, Integer y);
+	Integer sub(Integer x, Integer y);
+	Integer mul(Integer x, Integer y);
+	Integer div(Integer x, Integer y);
+}
