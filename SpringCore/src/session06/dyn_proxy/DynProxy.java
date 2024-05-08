b649852de66e0711473c18f419a20b0dc8dbaf1c
NTPU <NTPU@503-tr>
2024-05-08 11:01:52 +0800

    proxy

--- SpringCore/src/session06/dyn_proxy/DynProxy.java
+++ SpringCore/src/session06/dyn_proxy/DynProxy.java
@@ -1,5 +1,9 @@
 package session06.dyn_proxy;
 
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
 // 動態代理
 public class DynProxy {
 	
@@ -14,6 +18,16 @@ public class DynProxy {
 	public Object getProxy() {
 		Object proxyObj = null;
 		
+		// 1. 載入類別器
+		ClassLoader loader = object.getClass().getClassLoader();
+		// 2. 被代理的物件所實作的介面
+		Class<?>[] interfaces = object.getClass().getInterfaces();
+		// 3. 處理代理的實現
+		InvocationHandler handler = (Object proxy, Method method, Object[] args) -> {
+			return null;
+		};
+		// 4. 得到代理物件
+		proxyObj = Proxy.newProxyInstance(loader, interfaces, handler);
 		return proxyObj;
 	}
 	
