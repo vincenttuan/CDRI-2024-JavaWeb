033b54d430d48a117a1642052d7a7c40cc18c6a0
NTPU <NTPU@503-tr>
2024-05-08 14:37:00 +0800

    aop

--- SpringCore/src/session06/aop/MyLoggerAspect.java
+++ SpringCore/src/session06/aop/MyLoggerAspect.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
@@ -12,8 +13,14 @@ import org.springframework.stereotype.Component;
 @Aspect // 告訴 Spring 此為切面程式
 //@Order(1) // 執行順序(數字越小越先執行)
 public class MyLoggerAspect {
+	// 切點的方法
+	@Pointcut(value = "execution(* session06.aop.ComputerImpl.*(..))")
+	public void pt1() {}
 	
-	@Before(value = "execution(public Integer session06.aop.ComputerImpl.add(Integer, Integer))")
+	//@Before(value = "execution(public Integer session06.aop.ComputerImpl.add(Integer, Integer))")
+	//@Before(value = "execution(* session06.aop.ComputerImpl.add(..))")
+	//@Before(value = "execution(* session06.aop.ComputerImpl.*(..))")
+	@Before(value = "pt1()")
 	public void beforeAdvice(JoinPoint joinPoint) {
 		String methodName = joinPoint.getSignature().getName(); // 取得方法名稱
 		Object[] args = joinPoint.getArgs();
