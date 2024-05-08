0c742863ec46ac1656db38e2ac37f69008b7a251
NTPU <NTPU@503-tr>
2024-05-08 10:33:10 +0800

    proxy

--- SpringCore/src/session06/proxy/PersonProxy.java
+++ SpringCore/src/session06/proxy/PersonProxy.java
@@ -10,11 +10,16 @@ public class PersonProxy implements Person {
 	
 	@Override
 	public void work() {
-		// 公用邏輯
+		// before:公用邏輯
 		System.out.println("出門戴口罩");
 		// 執行代理對象的業務方法
-		person.work();
-		// 公用邏輯
+		try {
+			person.work();
+		} catch(Exception e) {
+			// exception:公用邏輯
+			System.out.println("發生錯誤, 處理方式是將錯誤 log 記錄下來");
+		}
+		// end:公用邏輯
 		System.out.println("回家要洗手");
 	}
 }
