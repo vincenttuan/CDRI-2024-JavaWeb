819a43be8fcb41ea2d96d2dc0ea21524a5223998
NTPU <NTPU@503-tr>
2024-05-08 11:28:10 +0800

    proxy

--- SpringCore/src/session06/dyn_proxy/TestDynProxy.java
+++ SpringCore/src/session06/dyn_proxy/TestDynProxy.java
@@ -8,8 +8,8 @@ public class TestDynProxy {
 
 	public static void main(String[] args) {
 		Calc calc = (Calc)new DynProxy(new CalcImpl()).getProxy();
-		System.out.println(calc.add(10, 5));
-		System.out.println(calc.div(10, 5));
+		System.out.println(calc.add(18, 56));
+		System.out.println(calc.div(43, 12));
 		
 		Person man = (Person)new DynProxy(new Man()).getProxy();
 		man.work();
