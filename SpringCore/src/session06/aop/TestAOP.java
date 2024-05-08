bbe41d07eb1f59cc42aec49584de072d7bd5c549
NTPU <NTPU@503-tr>
2024-05-08 15:14:12 +0800

    aop

--- SpringCore/src/session06/aop/TestAOP.java
+++ SpringCore/src/session06/aop/TestAOP.java
@@ -9,9 +9,9 @@ public class TestAOP {
 	public static void main(String[] args) {
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("aop-config.xml");
 		Computer computer = ctx.getBean("computerImpl", ComputerImpl.class);
-		System.out.println(computer.add(20, 10));
-		System.out.println(computer.sub(30, 10));
-		System.out.println(computer.mul(50, 10));
+		//System.out.println(computer.add(20, 10));
+		//System.out.println(computer.sub(30, 10));
+		//System.out.println(computer.mul(50, 10));
 		System.out.println(computer.div(60, 10));
 		
 	}
