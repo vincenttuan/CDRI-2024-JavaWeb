978e4f498c4bf6d51c507fed939726c5b832914e
NTPU <NTPU@503-tr>
2024-05-08 10:17:24 +0800

    proxy

--- SpringCore/src/test/TestPerson.java
+++ SpringCore/src/test/TestPerson.java
@@ -2,14 +2,15 @@ package test;
 
 import session06.proxy.Man;
 import session06.proxy.Person;
+import session06.proxy.PersonProxy;
 import session06.proxy.Woman;
 
 public class TestPerson {
 	public static void main(String[] args) {
-		Person man = new Man();
+		Person man = new PersonProxy(new Man());
 		man.work();
 		
-		Person woman = new Woman();
+		Person woman = new PersonProxy(new Woman());
 		woman.work();
 	}
 }
