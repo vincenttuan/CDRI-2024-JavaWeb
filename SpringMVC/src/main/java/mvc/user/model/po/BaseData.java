7a6e5c62b79cd5c4ed83dd70950a60fedfbc4993
NTPU <NTPU@503-tr>
2024-05-10 11:14:46 +0800

    mvc

--- SpringMVC/src/main/java/mvc/user/model/po/BaseData.java
+++ SpringMVC/src/main/java/mvc/user/model/po/BaseData.java
@@ -1,5 +1,12 @@
 package mvc.user.model.po;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class BaseData {
 	private Integer itemId;
 	private String itemName;
