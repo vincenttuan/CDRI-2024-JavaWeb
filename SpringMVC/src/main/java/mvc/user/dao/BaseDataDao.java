a5a09c0306375e2b712ae10052761642ba6c9d4c
NTPU <NTPU@503-tr>
2024-05-10 13:38:42 +0800

    mvc

--- SpringMVC/src/main/java/mvc/user/dao/BaseDataDao.java
+++ SpringMVC/src/main/java/mvc/user/dao/BaseDataDao.java
@@ -15,6 +15,6 @@ public interface BaseDataDao {
 	
 	List<Interest> findAllInterests(); // 查詢所有興趣
 	Interest getInterestById(Integer id); // 查詢興趣
-	int addInterest(Integer userId, Integer interestId); // 新增興趣
-	int deleteInterestsByUserId(Integer userId); // 刪除指定使用者的興趣
+	int addInterest(Integer userId, Integer interestId); // 新增使用者的興趣
+	int deleteInterestsByUserId(Integer userId); // 刪除使用者的興趣
 }
