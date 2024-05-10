0e3ef58b331004203d18eeb02df3ee96331e0412
NTPU <NTPU@503-tr>
2024-05-10 12:25:36 +0800

    mvc

--- SpringMVC/src/main/java/mvc/user/dao/UserDaoImpl.java
+++ SpringMVC/src/main/java/mvc/user/dao/UserDaoImpl.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
 import mvc.user.model.po.User;
@@ -24,8 +26,17 @@ public class UserDaoImpl implements UserDao {
 		String sql = "insert into user(name, age, birth, resume, education_id, gender_id) values(?, ?, ?, ?, ?, ?)";
 		//return jdbcTemplate.update(sql, user.getName(), user.getAge(), user.getBirth(), user.getResume(), user.getEducationId(), user.getGenderId());
 		
+		// 自動將 user 物件的屬性值給 sql 參數(?)使用
 		BeanPropertySqlParameterSource params = new BeanPropertySqlParameterSource(user);
-		return namedParameterJdbcTemplate.update(sql, params);
+		
+		// KeyHolder
+		KeyHolder keyHolder = new GeneratedKeyHolder();
+		
+		// keyHolder, new String[] {"id"} 將主鍵欄位 id 所自動生成的序號放到 keyHolder 中
+		namedParameterJdbcTemplate.update(sql, params, keyHolder, new String[] {"id"});
+		
+		int id = keyHolder.getKey().intValue(); // 最新新增紀錄的 user id
+		return id;
 	}
 
 	@Override
