0d260d5cb76b5cf41137386a3d74cefc94c96f9d
NTPU <NTPU@503-tr>
2024-05-10 13:37:10 +0800

    mvc

--- SpringMVC/src/main/java/mvc/user/dao/BaseDataDao.java
+++ SpringMVC/src/main/java/mvc/user/dao/BaseDataDao.java
@@ -7,13 +7,14 @@ import mvc.user.model.po.Gender;
 import mvc.user.model.po.Interest;
 
 public interface BaseDataDao {
-	List<Education> findAllEducations();
-	Education getEducationById(Integer id);
+	List<Education> findAllEducations(); // 查詢所有教育程度
+	Education getEducationById(Integer id); // 查詢教育程度
 	
-	List<Gender> findAllGenders();
-	Gender getGenderById(Integer id);
+	List<Gender> findAllGenders(); // 查詢所有性別
+	Gender getGenderById(Integer id); // 查詢性別
 	
-	List<Interest> findAllInterests();
-	Interest getInterestById(Integer id);
-	int addInterest(Integer userId, Integer interestId);
+	List<Interest> findAllInterests(); // 查詢所有興趣
+	Interest getInterestById(Integer id); // 查詢興趣
+	int addInterest(Integer userId, Integer interestId); // 新增興趣
+	int deleteInterestsByUserId(Integer userId); // 刪除指定使用者的興趣
 }
