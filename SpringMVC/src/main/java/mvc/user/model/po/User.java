0420e3bb6553d06de041abd76af126d38eb452f8
NTPU <NTPU@503-tr>
2024-05-14 11:01:02 +0800

    valid

--- SpringMVC/src/main/java/mvc/user/model/po/User.java
+++ SpringMVC/src/main/java/mvc/user/model/po/User.java
@@ -20,29 +20,29 @@ import lombok.NoArgsConstructor;
 public class User {
 	private Integer id; // 序號
 	
-	@NotEmpty (message = "姓名不可以是空白")
-	@Size(min = 2, max = 50, message = "姓名必須介於 {min} ~ {max} 字之間")
+	@NotEmpty (message = "{user.name.notempty}")
+	@Size(min = 2, max = 50, message = "{user.name.size}")
 	private String name; // 姓名
 	
-	@NotNull(message = "年齡不可以是空白")
-	@Range(min = 18, max = 150, message = "年齡需介於 {min} ~ {max} 歲之間")
+	@NotNull(message = "{user.age.notenull}")
+	@Range(min = 18, max = 150, message = "{user.age.range}")
 	private Integer age; // 年齡
 	
-	@NotNull(message = "日期不可以是空白")
-	@Past(message = "生日不可以大於今日日期")
+	@NotNull(message = "{user.birth.notnull}")
+	@Past(message = "{user.birth.past}")
 	@DateTimeFormat(pattern = "yyyy-MM-dd") // 日期格式
 	private Date birth; // 生日
 	
-	@Size(max = 1000, message = "履歷不可以超過 {max} 個字")
+	@Size(max = 1000, message = "{user.resume.size}")
 	private String resume; // 履歷
 	
-	@NotNull(message = "請選擇教育程度")
+	@NotNull(message = "{user.educationId.notnull}")
 	private Integer educationId; // 教育程度 id
 	
-	@NotNull(message = "請選擇性別")
+	@NotNull(message = "{user.sexId.notnull}")
 	private Integer genderId; // 性別 id
 	
-	@Size(min = 1, message = "興趣至少要選 {min} 個")
+	@Size(min = 1, message = "{user.interestIds.size}")
 	private Integer[] interestIds; // 興趣 ids
 	
 }
