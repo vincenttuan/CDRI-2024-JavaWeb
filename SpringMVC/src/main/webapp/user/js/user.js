628f0313aa69b162cdfd16d6fdf8001fdd142c68
NTPU <NTPU@503-tr>
2024-05-15 12:24:27 +0800

    rest

--- SpringMVC/src/main/webapp/user/js/user.js
+++ SpringMVC/src/main/webapp/user/js/user.js
@@ -4,7 +4,7 @@ const $ = (id) => document.getElementById(id);
 
 // 定義一個非同步函數來加載 HTML 內容
 const loadHTML = async(url, containerId) => {
-	const fullUrl = 'http://localhost:8080/SpringMVC/user' + url;
+	const fullUrl = 'http://localhost:8080/SpringMVC' + url;
 	try {
 		const response = await fetch(fullUrl); // 等待 fetch 請求完成
 		const data = await response.text(); // 等待回應本文內容
@@ -14,11 +14,30 @@ const loadHTML = async(url, containerId) => {
 	}
 };
 
+// 渲染 User 資料配置
+const renderUser = () => ``;
+
+// 資料渲染
+const fetchAndRenderData = async(url, containerId, renderFn) => {
+	const fullUrl = 'http://localhost:8080/SpringMVC' + url;
+	try {
+		const response = await fetch(fullUrl); // 等待 fetch 請求完成
+		const {state, message, data} = await response.json(); // 等待回應本文內容
+		console.log(state, message, data);
+		
+	} catch(e) {
+		console.error(e);
+	} 
+}; 
+
 // 待 DOM 加載完成之後再執行
 document.addEventListener("DOMContentLoaded", async() => {
 	
 	// 加上 await 關鍵字等待 loadHTML 函數完成才會進行下一個程序
-	await loadHTML('/user-form.html', 'user-form-container');
-	await loadHTML('/user-list.html', 'user-list-container');
+	await loadHTML('/user/user-form.html', 'user-form-container');
+	await loadHTML('/user/user-list.html', 'user-list-container');
+	
+	// 資料渲染(fetch取資料+渲染)
+	fetchAndRenderData('/mvc/rest/user', 'user-list-body', renderUser);
 	
 });
